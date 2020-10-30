b8e3642f771f1a9343fe136ddbca8871d2c47787
kranthi pyreddy <kra330@gmail.com>
2020-10-30 08:51:30 -0400

    Part A - Lab 3

--- js/chapter08-project01.js
+++ js/chapter08-project01.js
@@ -1,3 +1,22 @@
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+   
+   var subtotal;
+   var tax;
+   var shippingCost;
+   var grandTotal;
 
+   function loop() {
+       var i;
+       subtotal = 0;
+       tax = 0;
+       for (i = 0; i < filenames.length; i++) {
+           var total = quantities[i] * prices[i];
+           outputCartRow(filenames[i], titles[i], quantities[i], prices[i], total.toFixed(2));
+           subtotal = subtotal + total;
+       }
+       subtotal = subtotal;
+       tax = (subtotal * 0.10);
+       shippingCost = shippingCost(subtotal);
+       grandTotal = subtotal + tax + shippingCost;
+   }
--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,28 @@
-/* define functions here */
+/* define functions here 
 
+function calculateTotal(quantities, prices) {
+    for (i = 0; i < quantities.length; i++) {
+        return quantities[i] * prices[i];
+} 
+*/
+function outputCartRow(file, title, quantity, price, total) {
 
-        
+    var row =
+        "<tr>" +
+        "<td> <img src = images/" + file + "> </img> </td>" +
+        "<td> " + title + "</td> " +
+        "<td> " + quantity + " </td> " +
+        "<td> $" + price + " </td> " +
+        "<td> $" + total + " </td> " +
+        " </tr>";
+    document.write(row);
+}
+
+function shippingCost(subtotal) {
+    var Cost = 40;
+    if (subtotal > 1000) {
+        Cost = 0;
+    } 
+    return Cost;
+}
+
