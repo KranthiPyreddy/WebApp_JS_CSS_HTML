76398556d44ddbf2f2f79286f5b753009b122b42
kranthi pyreddy <kra330@gmail.com>
2020-11-01 05:43:09 -0500

    Part B - Lab 3

--- Lab3PartBStarter/js/data.js
+++ Lab3PartBStarter/js/data.js
@@ -1,1 +1,28 @@
 /* define your array of object literals here */
+countries =[
+        {
+            Name: "Canada",
+            Continent: "North America",
+            Cities: ["Calgary", "Montreal", "Toronto"],
+            Photos: ["images/canada1.jpg", "images/canada2.jpg", "images/canada3.jpg"]
+        },
+        {
+            Name: "United States",
+            Continent: "North America",
+            Cities: ["Boston", "Chicago", "New York", "Seattle", "Washington"],
+            Photos: ["images/us1.jpg", "images/us2.jpg"]
+        },
+        {
+            Name: "Italy",
+            Continent: "Europe",
+            Cities: ["Florence", "Milan", "Naples", "Rome"],
+            Photos: ["images/italy1.jpg", "images/italy2.jpg", "images/italy3.jpg", "images/italy4.jpg","images/italy5.jpg","images/italy6.jpg"]
+        },  
+        {
+            Name: "Spain",
+            Continent: "Europe",
+            Cities: ["Almeria", "Barcelona", "Madrid"],
+            Photos: ["images/spain1.jpg", "images/spain2.jpg"]
+        },
+    ]
+
--- Lab3PartBStarter/js/functions.js
+++ Lab3PartBStarter/js/functions.js
@@ -1,1 +1,31 @@
 /* add in your functions here */
+function outputCountryBox(name, continent, cities, photos) {
+    document.write("<div class='item'>" + 
+    "<h2>" + name + "</h2>" + 
+    "<p>" + continent + "</p>")
+    this.cities(cities);
+    this.photos(photos);
+    document.write(("<button>" + 'Visit' + "</button>") + 
+    "</div>");  
+}
+
+function logic() {
+    for (var i = 0; i < countries.length; i++) {
+        outputCountryBox(countries[i].Name, countries[i].Continent, countries[i].Cities, countries[i].Photos);
+    }
+};
+function cities(data) {(
+    document.write("<div class='inner-box'>" + ("<h3>" + "Cities" + "</h3>") + "<ul>"))
+    for (var i = 0; i < data.length; i++) {
+        //console.log(data[i]);
+        document.write("<li>" + data[i] + "</li>");
+    }
+    document.write("</ul>" + "</div>");
+}
+function photos(picData) {
+    document.write('<div class="inner-box">' + ("<h3>" + "Popular Photos" + "</h3>"));
+    for (var i = 0; i < picData.length; i++) {
+        document.write("<img src =" + picData[i] + "> </img>");
+    }
+    document.write("</div>");
+}
