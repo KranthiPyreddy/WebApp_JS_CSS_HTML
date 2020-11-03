e1c4cfa800412c1259b906419a7cd31ef554ad4b
kranthi pyreddy <kra330@gmail.com>
2020-11-03 11:12:02 -0500

    Part b - Lab 3 final

--- Lab3PartBStarter/js/functions.js
+++ Lab3PartBStarter/js/functions.js
@@ -2,28 +2,27 @@
 function outputCountryBox(name, continent, cities, photos) {
     document.write("<div class='item'>" + 
     "<h2>" + name + "</h2>" + 
-    "<p>" + continent + "</p>")
-    cities(cities);
-    photos(photos);
-    document.write(("<button>" + 'Visit' + "</button>") + 
-    "</div>"); 
-    function cities(data) {(
-        document.write("<div class='inner-box'>" + ("<h3>" + "Cities" + "</h3>") + "<ul>"))
+    "<p>" + continent + "</p>");
+    outputCities(cities);
+    outputPhotos(photos);
+    function outputCities(data) {
+        console.log(data);
+        document.write("<div class='inner-box'>" + ("<h3>" + "Cities" + "</h3>") + "<ul>");
         for (var i = 0; i < data.length; i++) {
             //console.log(data[i]);
             document.write("<li>" + data[i] + "</li>");
         }
         document.write("</ul>" + "</div>");
     }
-    function photos(picData) {
-        document.write('<div class="inner-box">' + ("<h3>" + "Popular Photos" + "</h3>"));
+    function outputPhotos(picData) {
+        document.write('<div class="inner-box"><h3>Popular Photos</h3>');
         for (var i = 0; i < picData.length; i++) {
             document.write("<img src =" + picData[i] + "> </img>");
         }
         document.write("</div>");
     } 
+    document.write("<button>Visit</button></div>"); 
 }
-
 function logic() {
     for (var i = 0; i < countries.length; i++) {
         outputCountryBox(countries[i].Name, countries[i].Continent, countries[i].Cities, countries[i].Photos);
